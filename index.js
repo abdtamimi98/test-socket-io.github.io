dc16dbcb8604fe0301ccc6442d045d06eed2aa5b
AbdelRahman Tamimi <atamimi@nixpend.com>
2021-01-31 22:54:15 +0200

    meh

---
+++ index.js
@@ -0,0 +1,17 @@
+const app = require('express')()
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    socket.console()
+    socket.on('chat message', (msg) => {
+      io.emit('chat message', msg);
+    });
+  });
+http.listen(6969, () => {
+  console.log('listening on *:6969');
+});
